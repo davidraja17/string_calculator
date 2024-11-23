eae8135a8dab7b569556cdc59acf8c42455c9df0
davidraja17 <david88raja@gmail.com>
2024-11-23 08:49:18 +0530

    spec coverage for single number input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,18 @@ RSpec.describe StringCalculator do
   let(:calculator) { StringCalculator.new }
   
   describe "#add" do
+
+    context "when the input is an empty string" do
+        it "returns 0" do
+        expect(calculator.add("")).to eq(0)
+        end
+    end
+
+    context "when the input has a single number" do
+        it "returns the number itself" do
+          expect(calculator.add("5")).to eq(5)
+        end
+    end
+
   end
 end
